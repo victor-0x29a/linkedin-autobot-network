381ec6f9f1e576d65c903efeb1ecb2c86c45441f
Victor <121402298+supppus@users.noreply.github.com>
2023-03-06 12:13:08 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,4 @@
+/* PT-BR */
 let [i, limite, conteudo] = [
     520,
     99999,
@@ -25,6 +26,47 @@ let [i, limite, conteudo] = [
       console.log("Erro - Botao nao encontrado");
       try {
     document.querySelector('button[aria-label="Entendi"]').click();
+      } catch (e) {
+        conteudo.scrollIntoView({
+          behavior: "smooth",
+          block: "end",
+          inline: "nearest",
+        });
+        // Skip
+      }
+      // Erro
+    }
+  }, 400);
+
+
+/* EN */
+let [i, limite, conteudo] = [
+    520,
+    99999,
+    document.querySelector(".scaffold-finite-scroll__content"),
+  ];
+  let intervalo = setInterval(() => {
+    if (i >= limite) {
+      console.log("Finalizou a automacao " + limite);
+      clearInterval(intervalo);
+    }
+    i++;
+    try {
+      let botao = document.querySelector(`#ember${i}`);
+      if (botao.ariaLabel.toLowerCase().includes("connect")) {
+        console.log(
+          botao.ariaLabel
+            .toLowerCase()
+            .replace("connect", "")
+            .replace("to", "")
+            .replace("to invite", "") + " Send"
+        );
+        botao.click();
+      }
+    } catch (e) {
+      console.log("Error - Not found button");
+      try {
+    document.querySelector('button[aria-label="I understood"]').click();
       } catch (e) {
         conteudo.scrollIntoView({
           behavior: "smooth",
