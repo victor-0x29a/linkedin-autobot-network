e3d3ceac456a29919429fb08f9655534cd5fc60e
Gabriel Logan <96627244+gabriel-logan@users.noreply.github.com>
2023-07-17 00:54:15 -0300

    Versão alternativa, so seguir o exemplo do readme

--- alternative.js
+++ alternative.js
@@ -1,41 +1,34 @@
-// OBS: ESSe aqui não foi feito um stop quando atingir o limite ainda então quando o limite for atingido, tem q atualizar a pagina para o codigo parar de rodar
-// IMPORTANTE
-
-
-// VERSAO ATUAL não testada
+// VERSAO ATUAL não testada ainda...
 
 let divs = document.querySelectorAll('.mt2');
-
-divs.forEach((div, index) => {
-  let botoes = div.querySelector('button');
-  let delay = 7000 // Atraso de 7 segundos
-  
-  setTimeout(() => {
-    console.log(
-      botoes.ariaLabel
-        .toLowerCase()
-        .replace("conectar", "")
-        .replace("para se", "")
-        .replace("convidar", "") + " Enviou solicitação"
-    );
-    botoes.click();
-  }, delay);
-});
-
-// VERSAO ANTIGA ABAIXO, NAO USE POIS NÂO ESTA CONCLUIDA
-
-setInterval(()=>{
-  let divs = document.querySelectorAll('.mt2');
-
-divs.forEach(div => {
-  let botoes = div.querySelector('button');
-  console.log(
-    botoes.ariaLabel
-      .toLowerCase()
-      .replace("conectar", "")
-      .replace("para se", "")
-      .replace("convidar", "") + " Enviou solicitação"
-  );
-  botoes.click();
-});
-}, 7000)
+function executaMissaoDosConectados() {
+  let count = 0; // contador de iterações
+  const numMaxIteration = 100; // Numero maximo de iterações, recomendo deixar 100
+  divs.forEach((div, index) => {
+    if (count < numMaxIteration) { // Verifica se o limite de 100 iterações ainda não foi atingido
+      let botoes = div.querySelector('button');
+      const delay = 7000; // Atraso de 7 segundos
+      setTimeout(() => {
+        console.log(
+          botoes.ariaLabel
+            .toLowerCase()
+            .replace("conectar", "")
+            .replace("para se", "")
+            .replace("convidar", "") + " Enviou solicitação"
+        );
+        botoes.click();
+      }, delay);
+      count++; // Incrementa o contador de iterações
+      console.log(`Enviou conexao ${count}`);
+      if(divs.length === count){
+        console.log('Acabou, a quantidade de divs é menor que o limite maximo \n');
+        console.log('Se precisar, atualize a pagina e reexecute o script');
+        return
+      }
+    } else {
+      console.log(`Limite de ${numMaxIteration} iterações atingido`);
+      return; // Interrompe o loop forEach
+    }
+  });
+}
+executaMissaoDosConectados();
