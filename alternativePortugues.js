681b0c6fb5a40b1114bb5701b6577625827b0498
Gabriel Logan <96627244+gabriel-logan@users.noreply.github.com>
2023-07-17 18:48:58 -0300

    Alternative version, use the same way as the original

---
+++ alternativePortugues.js
@@ -0,0 +1,47 @@
+// VERSAO ATUAL 
+//INFO-PT: Apenas copie uma das versoes, ingles ou portugues, editar o tempo que voce quer que seja feito as requisicoes, atualmente é 7 por segundo
+//PORTUGUES // testado e funcionando, porem não foi tratado os erros
+
+//copie apenas o código que você vai usar
+
+// DAQ ************
+let divs = document.querySelectorAll('.mt2');
+let count = 0; // contador de iterações
+const numMaxIteration = 50; // Numero maximo de iterações, recomendo deixar 50, se precisar atualize e faça de novo
+const delay = 7000; // Atraso de 7 segundos para cada requisição, não recomendado valores menores que 1000
+function executaMissaoDosConectados() {
+  if (count < numMaxIteration && count < divs.length) {
+    try {
+      let div = divs[count];
+      let botoes = div.querySelector('button');
+      console.log(
+        botoes.ariaLabel
+          .toLowerCase()
+          .replace("conectar", "")
+          .replace("para se", "")
+          .replace("convidar", "") + " Enviou solicitação"
+      );
+      botoes.click();
+      count++; // Incrementa o contador de iterações
+      console.log(`Enviou conexao ${count} \n`);
+    } catch (error) {
+      console.log('ERRO DESCONHECIDO \n\n');
+      console.log(error);
+      console.log('\n\n');
+    }
+    if (count === divs.length) {
+      console.log('Acabou, a quantidade de divs é menor que o limite maximo \n');
+      console.log('Se precisar, atualize a pagina e reexecute o script \n\n');
+      console.log(`Tempo que levou a execução: ${(delay * divs.length) / 60} minutos ou ${delay * divs.length} segundos - byLogan \n`);
+      return;
+    } else {
+      setTimeout(executaMissaoDosConectados, delay);
+    }
+  } else {
+    console.log(`Limite de ${numMaxIteration} iterações atingido \n`);
+    console.log(`Tempo que levou a execução aproximadamente: ${Math.round((delay * divs.length) / 60)} minutos - byLogan \n`);
+    return; // Interrompe a execução
+  }
+}
+executaMissaoDosConectados();
+// ATE AQUI ***************
