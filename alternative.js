1e0424441dfb0a4f5bad4aee4117bc43bc51e4f3
Gabriel Logan <96627244+gabriel-logan@users.noreply.github.com>
2023-07-17 01:04:26 -0300

    Versão alternativa, utilize do mesmo jeito que a original

--- alternative.js
+++ alternative.js
@@ -6,27 +6,33 @@ function executaMissaoDosConectados() {
   const numMaxIteration = 100; // Numero maximo de iterações, recomendo deixar 100
   divs.forEach((div, index) => {
     if (count < numMaxIteration) { // Verifica se o limite de 100 iterações ainda não foi atingido
-      let botoes = div.querySelector('button');
-      const delay = 7000; // Atraso de 7 segundos para cada requisição
-      setTimeout(() => {
-        console.log(
-          botoes.ariaLabel
-            .toLowerCase()
-            .replace("conectar", "")
-            .replace("para se", "")
-            .replace("convidar", "") + " Enviou solicitação"
-        );
-        botoes.click();
-      }, delay);
-      count++; // Incrementa o contador de iterações
-      console.log(`Enviou conexao ${count}`);
-      if(divs.length === count){
+      try {
+        let botoes = div.querySelector('button');
+        const delay = 7000; // Atraso de 7 segundos para cada requisição, não recomendado valores menores que 1000
+        setTimeout(() => {
+          console.log(
+            botoes.ariaLabel
+              .toLowerCase()
+              .replace("conectar", "")
+              .replace("para se", "")
+              .replace("convidar", "") + " Enviou solicitação"
+          );
+          botoes.click();
+        }, delay);
+        count++; // Incrementa o contador de iterações
+        console.log(`Enviou conexao ${count} \n`);
+      } catch (error) {
+        console.log('ERRO DESCONHECIDO \n\n');
+      }
+      if(divs.length == count){
         console.log('Acabou, a quantidade de divs é menor que o limite maximo \n');
-        console.log('Se precisar, atualize a pagina e reexecute o script');
+        console.log('Se precisar, atualize a pagina e reexecute o script \n\n');
+        console.log(`Tempo que levou a execução: ${(delay*divs.length)/60}minutos ou ${delay*divs.length} segundos \n`);
         return
       }
     } else {
-      console.log(`Limite de ${numMaxIteration} iterações atingido`);
+      console.log(`Limite de ${numMaxIteration} iterações atingido \n`);
+      console.log(`Tempo que levou a execução aproximadamente: ${Math.round((delay * divs.length) / 60)} minutos \n`);
       return; // Interrompe o loop forEach
     }
   });
