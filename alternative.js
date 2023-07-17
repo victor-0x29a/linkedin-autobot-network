f3bd95e157af0d590fb6df7d3b11eef8bd1a6009
Gabriel Logan <96627244+gabriel-logan@users.noreply.github.com>
2023-07-17 00:19:22 -0300

    Add files via upload

--- alternative.js
+++ alternative.js
@@ -1,6 +1,29 @@
 // OBS: ESSe aqui não foi feito um stop quando atingir o limite ainda então quando o limite for atingido, tem q atualizar a pagina para o codigo parar de rodar
 // IMPORTANTE
 
+
+// VERSAO ATUAL
+
+let divs = document.querySelectorAll('.mt2');
+
+divs.forEach((div, index) => {
+  let botoes = div.querySelector('button');
+  let delay = 7000 // Atraso de 7 segundos
+  
+  setTimeout(() => {
+    console.log(
+      botoes.ariaLabel
+        .toLowerCase()
+        .replace("conectar", "")
+        .replace("para se", "")
+        .replace("convidar", "") + " Enviou solicitação"
+    );
+    botoes.click();
+  }, delay);
+});
+
+// VERSAO ANTIGA ABAIXO, NAO USE POIS NÂO ESTA CONCLUIDA
+
 setInterval(()=>{
   let divs = document.querySelectorAll('.mt2');
 
